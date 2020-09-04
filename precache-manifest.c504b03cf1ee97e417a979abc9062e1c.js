14cfe0aae6e571a30dd87b07c4852ddaf9bcc275
yunjee18 <yunjee118@gmail.com>
2020-09-04 13:05:41 +0900

    Updates

---
+++ precache-manifest.c504b03cf1ee97e417a979abc9062e1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6ef36088eabcdcc33f4be636b49d3ea",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "68ac89cb533a67826cf4",
+    "url": "/movie_app/static/css/main.b9db12a2.chunk.css"
+  },
+  {
+    "revision": "2cb00f0b337cb1b1485d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68ac89cb533a67826cf4",
+    "url": "/movie_app/static/js/main.f14bf9f0.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
